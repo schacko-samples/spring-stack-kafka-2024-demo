1d8065b876acbad837aa8ad486e83f6ec65069ec
Soby Chacko <soby.chacko@broadcom.com>
2024-04-02 14:17:15 -0400

    SK app 4

---
+++ spring-kafka-app4/src/test/java/app4/SpringKafkaApp4Tests.java
@@ -0,0 +1,13 @@
+package app4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaApp4Tests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
